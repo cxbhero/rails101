165c54d72234af8f357afc881a358f9b0a0f3ed3
cxbhero <cxbhero@163.com>
2022-06-01 17:26:39 +0800

    add rest files

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  resources :groups do
+    member do
+      post :join
+      post :quit
+    end
+    resources :posts
+  end
+  namespace :account do
+    resources :groups
+    resources :posts
+  end
+  root 'groups#index'
+end
